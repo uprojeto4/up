8581df5618456c41fe82222d26cb6496f3fcbced
Brendon Girão <brendongirao@gmail.com>
2017-10-02 21:08:19 -0300

    ambiente preparado

--- app/src/main/java/br/ufc/quixada/up/MainActivity.java
+++ app/src/main/java/br/ufc/quixada/up/MainActivity.java
@@ -80,17 +80,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_anuncio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_negociacoes) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_lista_desejos) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_categorias) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_perfil) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_configuracoes) {
+
+        } else if (id == R.id.nav_sair) {
 
         }
 
