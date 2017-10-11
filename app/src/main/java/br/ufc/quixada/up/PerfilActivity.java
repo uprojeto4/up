18e9d3fe3d593a03d99d355bad55d59f3e38b45d
Brendon Girão <brendongirao@gmail.com>
2017-10-11 11:49:37 -0300

    perfil com avaliações e tabs

--- app/src/main/java/br/ufc/quixada/up/PerfilActivity.java
+++ app/src/main/java/br/ufc/quixada/up/PerfilActivity.java
@@ -4,15 +4,20 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.request.RequestOptions;
 
+import br.ufc.quixada.up.Adapters.perfilFragmentPagerAdapater;
 import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.GrayscaleTransformation;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
@@ -21,12 +26,22 @@ import static br.ufc.quixada.up.R.drawable.image_test_1;
 
 public class PerfilActivity extends BaseActivity {
 
+    private TabLayout perfilTabLayout;
+    private ViewPager perfilViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        perfilTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        perfilViewPager = (ViewPager) findViewById(R.id.view_pager_perfil);
+
+        perfilViewPager.setAdapter(new perfilFragmentPagerAdapater(getSupportFragmentManager(), getResources().getStringArray(R.array.tabs_perfil)));
+
+        perfilTabLayout.setupWithViewPager(perfilViewPager);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -53,12 +68,19 @@ public class PerfilActivity extends BaseActivity {
 //        Bitmap blurred = blurRenderScript(this, image_test_1, radiusArr[position]);//second parametre is radius
 //        imageview.setImageBitmap(blurred);
 
-        MultiTransformation multi = new MultiTransformation(
-                new BlurTransformation(25));
+//        MultiTransformation multi = new MultiTransformation(
+//                new BlurTransformation(25));
+//
+//        Glide.with(this).load(image_test_1)
+//                .apply(RequestOptions.bitmapTransform(multi))
+//                .into((ImageView) findViewById((R.id.header_cover_image)));
+    }
 
-        Glide.with(this).load(image_test_1)
-                .apply(RequestOptions.bitmapTransform(multi))
-                .into((ImageView) findViewById((R.id.header_cover_image)));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.perfil, menu);
+        return true;
     }
 
 }
